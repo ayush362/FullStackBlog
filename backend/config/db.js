ecfcf8cc20a2e6f1a8aeb8520a6f0b7ed84ab5c7
ayush362 <ayushkhatri362@gmail.com>
2024-12-07 14:15:06 +0530

    change the mysql from local server to online

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+const fs = require('fs');
+
+
+dotenv.config(); // Load environment variables from .env file
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "myuser",
-    password: "mypassword",
-    database: "mydb",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    connectTimeout: 1000000 
 });
 
 module.exports = db;
